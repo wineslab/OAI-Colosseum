175b00128c571257756d49da8bbcf8cb90d5410e
Leonardo Bonati <leonardo.bonati@gmail.com>
2024-03-18 21:38:06 -0400

    Trying to start OAI from start.sh script

--- radio_api/start.sh
+++ radio_api/start.sh
@@ -22,6 +22,13 @@ cd ${APP_DIR}
 if [ "$mode_type" == "core" ]; then
   route add -net 12.1.1.0/24 gw 192.168.70.134 dev demo-oai
   python3 ${APP_DIR}/${script_cmd} &
+else
+  systemctl stop oai_ran.service
+  echo "READY" > /tmp/NR_STATE
+  echo "[`date`] Starting 5G ${mode_type} service from start.sh" >> /logs/run.log
+  echo "[`date`] Command line ${script_cmd}" >> /logs/run.log
+  cd ${APP_DIR}
+  python3 ${APP_DIR}/${script_cmd}
 fi
 
 exit 0
