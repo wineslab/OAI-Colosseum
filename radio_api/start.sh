168d0295b2ea2fd8c87f060eb1dd4e63d7fbd22b
Leonardo Bonati <leonardo.bonati@gmail.com>
2024-10-18 09:23:01 -0400

    Fixed batch job start.sh

--- radio_api/start.sh
+++ radio_api/start.sh
@@ -33,6 +33,9 @@ else
   echo "READY" > /tmp/NR_STATE
   echo "[`date`] Starting 5G ${mode_type} service from start.sh" >> /logs/run.log
   echo "[`date`] Command line ${script_cmd}" >> /logs/run.log
+
+  cd ${APP_DIR}
+  python3 ${APP_DIR}/${script_cmd}
 fi
 
 exit 0
