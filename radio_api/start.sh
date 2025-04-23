6a782a25724ae5cb7d8ed4c272ba7b7b7988149b
Leonardo Bonati <leonardo.bonati@gmail.com>
2025-04-23 16:37:39 -0400

    Error handling

--- radio_api/start.sh
+++ radio_api/start.sh
@@ -31,7 +31,7 @@ else
   echo "[`date`] Starting 5G ${mode_type} service from start.sh" >> /logs/run.log
   echo "[`date`] Command line ${script_cmd}" >> /logs/run.log
   cd ${APP_DIR}
-  python3 ${APP_DIR}/${script_cmd}
+  python3 ${APP_DIR}/${script_cmd} 2> /logs/python_error.log
 fi
 
 exit 0
