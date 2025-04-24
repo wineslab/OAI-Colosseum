edb6c0842e86d3f2774e3f85c496c546ebd1028d
Leonardo Bonati <leonardo.bonati@gmail.com>
2025-04-24 19:04:36 -0400

    Trying to get core network IP with nmap scan

--- utils/set_route_to_cn.py
+++ utils/set_route_to_cn.py
@@ -96,20 +96,93 @@ def improved_arping(net, iface=None, timeout=2, verbose=False, retry=2):
     return result[0]
 
 
+def get_active_nodes_nmap(net, iface, max_trials=10) -> list:
+
+    logging.info('Getting list of active nodes using nmap')
+
+    # SRN IP offset. E.g., SRN 1 has IP .101
+    srn_offset = 100
+
+    # flag to check we found at least one node that is not SRN.
+    # Otherwise nmap has to be repeated
+    nmap_successful = False
+    re_runs = -1
+
+    nmap_host_keyword = 'Nmap scan report for '
+    nmap_up_keyword = 'Host is up'
+
+    # get net base IP: e.g., if net is 172.30.104.0/24, get 172.30.104.
+    net_last = net.split('.')[3]
+    net_base_ip = net[:-len(net_last)]
+    logging.info('net_last is {}'.format(net_last))
+    logging.info('net_base_ip is {}'.format(net_base_ip))
+
+    nmap_command = 'nmap -T4 -sn {}'.format(net)
+    logging.info('nmap command is {}'.format(nmap_command))
+
+    # list of active nodes IP addresses
+    active_nodes = []
+    while not nmap_successful:
+        # use a temporary list to store active nodes
+        tmp_nodes = []
+
+        re_runs += 1
+
+        if re_runs > max_trials:
+            logging.error('No active hosts found by nmap. Returning empty list')
+            return []
+
+        logging.info('Starting nmap')
+        pipe = subprocess.Popen(nmap_command, shell=True, stdout=subprocess.PIPE).stdout
+
+        # separate lines returned by the above command
+        lines = pipe.read().decode("utf-8").splitlines()
+
+        for l_idx in range(len(lines)):
+            curr_line = lines[l_idx]
+            logging.info('curr_line is {}'.format(curr_line))
+
+            # get SRN num from host IP
+            if nmap_host_keyword in curr_line and nmap_up_keyword in lines[l_idx + 1]:
+                srn_num = int(curr_line.split(col0_base_ip)[1]) - srn_offset
+
+                # check this is an actual SRN and add it to dictionary
+                if srn_num > 0:
+                    tmp_nodes.append(curr_line)
+                    logging.info('SRN with IP {} is active'.format(curr_line))
+                else:
+                    # this is not an SRN but is used to check that nmap is successful
+                    logging.info('nmap successful')
+                    nmap_successful = True
+
+    if len(tmp_nodes) > 0:
+        active_nodes = tmp_nodes
+    else:
+        logging.error('No active hosts found by nmap. Exiting')
+        exit(1)
+
+    logging.info('nmap completed, found active nodes {}'.format(active_nodes))
+    return active_nodes
+
+
 def scan_and_print_neighbors(net, interface, timeout=5):
     logging.info('Calling scan_and_print_neighbors function')
     output_scan_and_print = open('/logs/scan_print_output.log', "a")
     error_scan_and_print = open('/logs/scan_print_error.log', "a")
     try:
-        logging.info('About to send ARP requests')
+        logging.info('About to scan network for nodes')
         # ans, unans = scapy.layers.l2.arping(net, iface=interface, timeout=timeout, verbose=False)
-        ans, unans = improved_arping(net, iface=interface, timeout=timeout, verbose=True, retry=2)
-        logging.info('ARP requests sent')
-        logging.info('Got ans: {}'.format(ans.res))
-        logging.info('Got unans: {}'.format(unans.res))
-        for s, r in ans.res:
-            line = r.sprintf("%ARP.psrc%")
-            logging.info(line)
+        # ans, unans = improved_arping(net, iface=interface, timeout=timeout, verbose=True, retry=2)
+        ans = get_active_nodes_nmap(net, iface=interface)
+        logging.info('Done scanning network for nodes')
+        # logging.info('Got ans: {}'.format(ans.res))
+        # logging.info('Got unans: {}'.format(unans.res))
+        logging.info('Got ans: {}'.format(ans))
+        # for s, r in ans.res:
+        for line in ans:
+            # line = r.sprintf("%ARP.psrc%")
+            # logging.info(line)
+            logging.info('Loop node with IP {}'.format(line))
             command = ['route', 'add', '-net', '192.168.70.128/26', 'gw', line, 'dev', 'col0']
             response = subprocess.run(args=command, stdout=output_scan_and_print, stderr=error_scan_and_print).returncode
             command = ['ping', '-c', '1', '-t', '1', '192.168.70.129']
@@ -120,15 +193,15 @@ def scan_and_print_neighbors(net, interface, timeout=5):
                 break
             else:
                 os.system("route del -net 192.168.70.128/26")
-            try:
-                hostname = socket.gethostbyaddr(r.psrc)
-                logging.info('Got hostname {}'.format(hostname))
-                line += " " + hostname[0]
-                logging.info('Got line {}'.format(line))
-            except socket.herror as he:
-                # failed to resolve
-                logging.warning('Passing on error: {}'.format(he))
-                pass
+            # try:
+            #     hostname = socket.gethostbyaddr(r.psrc)
+            #     logging.info('Got hostname {}'.format(hostname))
+            #     line += " " + hostname[0]
+            #     logging.info('Got line {}'.format(line))
+            # except socket.herror as he:
+            #     # failed to resolve
+            #     logging.warning('Passing on error: {}'.format(he))
+            #     pass
     except socket.error as e:
         logging.error('Got error: {}'.format(e))
         if e.errno == errno.EPERM:     # Operation not permitted
