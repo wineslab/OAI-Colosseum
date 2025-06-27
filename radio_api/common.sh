a050dd415d603a464f498946bc7048953b9f597e
Leonardo Bonati <leonardo.bonati@gmail.com>
2025-06-27 11:36:05 -0400

    Added pcap capture on E2 interface

--- radio_api/common.sh
+++ radio_api/common.sh
@@ -51,6 +51,10 @@ if [ "$mode_type" == "gnb" ]; then
           ping -c 3 ${near_rt_ric_ip} &> /logs/ric_reachability.log
           echo "" >> /logs/ric_reachability.log
           ncat -zv ${near_rt_ric_ip} --sctp 32224 &>> /logs/ric_reachability.log
+
+          echo "Starting tcpdump process in background" >> /logs/ric_reachability.log
+          nohup tcpdump host ${near_rt_ric_ip} and port 32224 -w /logs/e2_gnb.pcap
+          echo "" >> /logs/ric_reachability.log
         fi
 
         if [ -z ${gnb_id+x} ]; then
